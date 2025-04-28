7055bd8acfd7af4360ead2e1fb19b0645ea0be21
Alex Oh <odange2119@gmail.com>
2025-04-29 00:20:13 +0900

    first commit55

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Homepage from './page/Homepage';
 import Aboutpage from './page/Aboutpage';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import ProductPage from './page/ProductPage';
+import ProductPage1 from './page/ProductPage1';
 import ProductDetailPage from './page/ProductDetailPage';
 import { useState } from 'react';
 import UserPage from './page/UserPage';
@@ -14,22 +14,16 @@ function App() {
   const PrivateRoute = () => {
     return authenticate == true?<UserPage/>:<Navigate to = "/login"/>
   }
-
   return (
     <div>
       <Routes>
-        {/* path는 각 페이지의 주소, element는 주소에 보여주고 싶은 페이지 */}
-        <Route path="" element={<Homepage />}/>
-        <Route path="/about" element={<Aboutpage />}/>
-        <Route path="/products" element={<ProductPage />}/>
-        <Route path="/products/:id" element={<ProductDetailPage />}/>
+        <Route path="/" element={ <Homepage />} />
+        <Route path="/about" element={ <Aboutpage />} />
+        <Route path="/Products" element={ <ProductPage1 />} />
+        <Route path="/Products/:id" element={ <ProductDetailPage />} />
         <Route path="/login" element={<LoginPage />}/>
         <Route path="/user" element={<PrivateRoute />}/>
-
       </Routes>
-
-      
-      
     </div>
   );
 }
