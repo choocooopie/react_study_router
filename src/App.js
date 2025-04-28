b37aa1a4c966d0ea4324d452d9283c5758559277
Alex Oh <odange2119@gmail.com>
2025-04-29 00:02:38 +0900

    first commit22

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Homepage from './page/Homepage';
+import Aboutpage from './page/Aboutpage';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import ProductPage from './page/ProductPage';
+import ProductDetailPage from './page/ProductDetailPage';
+import { useState } from 'react';
+import UserPage from './page/UserPage';
+import LoginPage from './page/LoginPage';
 
 function App() {
+  const [authenticate, setAuthenticate] = useState(false);
+  const PrivateRoute = () => {
+    return authenticate == true?<UserPage/>:<Navigate to = "/login"/>
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        {/* path는 각 페이지의 주소, element는 주소에 보여주고 싶은 페이지 */}
+        <Route path="" element={<Homepage />}/>
+        <Route path="/about" element={<Aboutpage />}/>
+        <Route path="/products" element={<ProductPage />}/>
+        <Route path="/products/:id" element={<ProductDetailPage />}/>
+        <Route path="/login" element={<LoginPage />}/>
+        <Route path="/user" element={<PrivateRoute />}/>
+
+      </Routes>
+
+      
+      
     </div>
   );
 }
